0a36def0a9815cdd3af56abba28ae6c28f9912b9
Kyle Montag <thekylemontag@gmail.com>
2017-05-02 23:33:38 -0400

    update to use firebase
    lots of other cool shit too

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const Slapp = require('slapp');
 const ConvoStore = require('slapp-convo-beepboop');
 const Context = require('slapp-context-beepboop');
 const slack = require('slack');
+const firebase = require('firebase');
+
+// initialize firebase
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_GGM_API_KEY,
+    authDomain: `${process.env.FIREBASE_GGM_PROJECT_ID}.firebaseapp.com`,
+    databaseURL: `https://${process.env.FIREBASE_GGM_DB_URL}.firebaseio.com`,
+    storageBucket: `${process.env.FIREBASE_GGM_STORAGE_BUCKET}.appspot.com`
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
 var port = process.env.PORT || 6000;
@@ -27,57 +38,86 @@ I will respond to the following messages:
 
 var event_count = 0;
 
-const getRandomEmoji = (msg) => {
-    const appToken = msg.meta.app_token;
+const getRandomEmoji = msg => {
+  const appToken = msg.meta.app_token;
 
-    return new Promise((resolve, reject) => {
-        const payload = {
-            token: appToken
-        };
-        slack.emoji.list(payload, (err, data) => {
-            const emoji = data['emoji'];
-            const items = Object.keys(emoji);
-            const item = items[Math.floor(Math.random()*items.length)];
-            resolve([item, emoji[item]]);
-        });
-    })
+  return new Promise((resolve, reject) => {
+    const payload = {
+      token: appToken
+    };
+    slack.emoji.list(payload, (err, data) => {
+      const emoji = data['emoji'];
+      const items = Object.keys(emoji);
+      const item = items[Math.floor(Math.random() * items.length)];
+      resolve([item, emoji[item]]);
+    });
+  });
 };
 
 const createEncounterMessage = (text, msg) => {
-    getRandomEmoji(msg).then((val) => {
-        const emojiName = val[0];
-        const emojiImage = val[1];
-        const slackMoji = `:${emojiName}:`;
+  getRandomEmoji(msg).then(val => {
+    const emojiName = val[0];
+    const emojiImage = val[1];
+    const slackMoji = `:${emojiName}:`;
 
-        msg.say({
-            channel: process.env.ENCOUNTER_CHANNEL_NAME || 'meme-hunting',
-            text: text,
-            attachments: [
-                {
-                    text: `A wild ${slackMoji} has appeared!`,
-                    fallback: val,
-                    callback_id: 'encounter_callback',
-                    actions: [
-                        { name: 'answer', text: 'Catch', type: 'button', value: `caught|${slackMoji}` },
-                        { name: 'answer', text: 'Run', type: 'button', value: `ran from|${slackMoji}` }
-                    ]
-                }
-            ]
-        });
+    msg.say({
+      channel: process.env.ENCOUNTER_CHANNEL_NAME || 'meme-hunting',
+      text: text,
+      attachments: [
+        {
+          text: `A wild ${slackMoji} has appeared!`,
+          fallback: val,
+          callback_id: 'encounter_callback',
+          actions: [
+            {
+              name: 'answer',
+              text: 'Catch',
+              type: 'button',
+              value: `caught|${slackMoji}`
+            },
+            {
+              name: 'answer',
+              text: 'Run',
+              type: 'button',
+              value: `ran from|${slackMoji}`
+            }
+          ]
+        }
+      ]
     });
+  });
+};
+
+const addEmojiToUser = (ref, emoji) => {
+    ref.transaction((user => {
+        if (user) {
+            if (user.memeventory && user.memeventory[emoji]) {
+                user.memeventory[emoji]++;
+            } else {
+                user.memeventory[emoji] = 1;
+            }
+        } else {
+            user = {
+                memeventory: {}
+            }
+            user.memeventory[emoji] = 1;
+        }
+        return user;
+    }));
 }
 
 const createEncounterCallback = () => {
-    slapp.action('encounter_callback', 'answer', (msg, value) => {
-        const parsedValue = value.split('|');
-        const command = parsedValue[0];
-        const emoji = parsedValue[1];
-        msg.respond(
-            msg.body.response_url,
-            `Congrats, ${msg.body.user.name}! You ${command} the wild ${emoji}!`
-        );
-    });
-}
+  slapp.action('encounter_callback', 'answer', (msg, value) => {
+    const parsedValue = value.split('|');
+    const command = parsedValue[0];
+    const emoji = parsedValue[1];
+      addEmojiToUser(db.ref(`users/${msg.body.user.id}`), emoji);
+    msg.respond(
+      msg.body.response_url,
+      `Congrats, ${msg.body.user.name}! You ${command} the wild ${emoji}!`
+    );
+  });
+};
 
 // this will need prefixing so that each encounter has its own callback
 // will help to prevent sonnie pls
@@ -87,9 +127,9 @@ const incrementEventCount = msg => {
   event_count++;
   // do logic for encounter here
   if (event_count % 5 === 0) {
-      event_count = 0;
-      getRandomEmoji(msg);
-      createEncounterMessage('ENCOUNTER', msg);
+    event_count = 0;
+    getRandomEmoji(msg);
+    createEncounterMessage('ENCOUNTER', msg);
   }
 };
 //*********************************************
@@ -107,21 +147,19 @@ slapp.message('event_count', ['direct_message'], msg => {
   incrementEventCount(msg);
 });
 
-// demonstrate returning an attachment...
-slapp.message('attachment', ['mention', 'direct_message'], msg => {
-  msg.say({
-    text: 'Check out this amazing attachment! :confetti_ball: ',
-    attachments: [
-      {
-        text: 'Slapp is a robust open source library that sits on top of the Slack APIs',
-        title: 'Slapp Library - Open Source',
-        image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-        title_link: 'https://beepboophq.com/',
-        color: '#7CD197'
-      }
-    ]
-  });
-  incrementEventCount(msg);
+slapp.message('memeventory', ['mention', 'direct_message'], msg => {
+    console.log(msg.body.event);
+    db.ref(`users/${msg.body.event.user}/memeventory`).once('value').then((snapshot) => {
+        const memeventory = snapshot.val();
+        console.log(memeventory);
+        const memeventoryHeader = Object.keys(memeventory).map(key => {
+            return `${key}`;
+        }).join(" ");
+        msg.say(memeventoryHeader)
+        const formattedMemes = Object.values(memeventory).map(emojiCount => `  ${emojiCount}  `).join(' ')
+        const memeventoryBody = `\`${formattedMemes}\``;
+        msg.say(memeventoryBody);
+    });
 });
 
 // increment the message count
